d70ac4c975145b81e821e1392e7e6060973b470d
Jpc <Joaop3595@gmail.com>
2025-05-16 19:54:32 -0300

    test: add jest to start the application tests

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Map '@/...' alias to the 'src' directory
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/'],
+};
+
+export default createJestConfig(config);
